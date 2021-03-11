ddace3ac51bbf48f17fbe294b189024d676302ae
Braxton Moon <braxton.moon@ttu.edu>
2021-03-11 01:23:14 +0000

    Included .idea files in gitignore for anyone using intellij
    	modified:   .gitignore

---
+++ src/LoginGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class LoginGUI {
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(100,100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        JPanel panel = new JPanel();
+
+    }
+}
